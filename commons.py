e8be1ea12ebdbf4de22221c058018a3d8344a9a6
sq3 <mail@mschnitzius.com>
2015-05-12 20:32:56 +0200

    Let's us use virtualenv. Otherwise it didn't work for me

--- commons.py
+++ commons.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 import csv
 from datetime import datetime
 # http://labix.org/python-dateutil
--- gapis.py
+++ gapis.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
 import httplib2
 from httplib import BadStatusLine
