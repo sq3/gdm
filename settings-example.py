2771c3392cfe472e65a02d3fa16185d81807e3a6
Marc Schnitzius <mail@mschnitzius.com>
2015-05-13 08:14:17 +0200

    Add an example settings.py for better undestanding whats to do

---
+++ settings-example.py
@@ -0,0 +1,23 @@
+# Copy your credentials from the console
+CLIENT_ID_SRC = '1234567890abcdefg.apps.googleusercontent.com'
+CLIENT_SECRET_SRC = '1a2b3c4f-ABCDEDF-ABC'
+
+CLIENT_ID_DST = '1234567890abcdefg.apps.googleusercontent.com'
+CLIENT_SECRET_DST = '1a2b3c4f-ABCDEDF-ABC'
+
+"""Email of the Service Account"""
+SERVICE_ACCOUNT_SRC = '123456789-abcdefg@developer.gserviceaccount.com'
+
+SERVICE_ACCOUNT_DST = '123456789-abcdefg@developer.gserviceaccount.com'
+
+"""Path to the Service Account's Private Key file"""
+SERVICE_ACCOUNT_PRIVATE_KEY_SRC = './src-gdrive.p12'
+
+SERVICE_ACCOUNT_PRIVATE_KEY_DST = './dst-gdrive.p12'
+
+# Check https://developers.google.com/drive/scopes for all available scopes
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+
+# Redirect URI for installed apps
+REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
