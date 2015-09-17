700f84de3e67428adb1579ab9830ccfb56766669
tschaefer <github@blackox.org>
2015-09-17 18:13:47 +0200

    service: set flask threaded, modified service method more friendly

--- app/service.py
+++ app/service.py
@@ -7,33 +7,23 @@ import gdm
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def service(name=None):
-    user_data = {}
+def service():
     if flask.request.method == 'POST':
-        user_data.update(
-                migrate_from=unicode(
-                    flask.request.form['migrate-from']
-                    )
-                )
-        
-        user_data.update(
-                client_id=unicode(
-                     flask.request.form['client-id'])
-                )
-        
-        user_data.update(
-                client_secret=unicode(
-                    flask.request.form['client-secrte']
-                    )
-                )
-        
-        user_data.update(
-                migrate_to=unicode(
-                    flask.requst.form['migrate-to']
-                    )
-                )
+        migrate_from = unicode(flask.request.form['migrate-from'])
+        client_id = unicode(flask.request.form['client-id'])
+        client_secret = unicode(flask.request.form['client-secret'])
+        migrate_to = unicode(flask.request.form['migrate-to'])
+
+        user_data = {
+                'migrate_form': migrate_from,
+                'client_id': client_id,
+                'client_secret': client_secret,
+                'migrate_to': migrate_to
+            }
+
+        print user_data
 
     return flask.render_template('index.html')
 
 def run():
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
