814f206c6bc5df4e0571874c7a95f444baedb2c6
sq3 <mail@mschnitzius.com>
2015-09-17 16:30:57 +0200

    setup some basic foo for flask and the world around me in the universe and the spacship to the π

---
+++ app/service.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import flask
+from flask import Flask
+import gdm
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def service(name=None):
+   user_data = {}
+   if flask.request.method == 'POST':
+        user_data.update(
+                migrate_from=unicode(
+                    flask.request.form['migrate-from']
+                    )
+                )
+
+        user_data.update(
+                client_id=unicode(
+                     flask.request.form['client-id'])
+                )
+
+        user_data.update(
+                client_secret=unicode(
+                    flask.request.form['client-secrte']
+                    )
+                )
+
+        user_data.update(
+                migrate_to=unicode(
+                    flask.requst.form['migrate-to']
+                    )
+                )
+
+   return flask.render_template('index.html')
+
+def run():
+    app.run(host='0.0.0.0', port=8080, debug=True)
