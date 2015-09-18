c3df5457c0a02401b9af62fa7b35b6a4229328bf
Marc Schnitzius <mail@mschnitzius.com>
2015-09-18 08:08:13 +0200

    now using database in service.py

--- app/service.py
+++ app/service.py
@@ -2,9 +2,13 @@
 
 import flask
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from models import Walker, Migration
 import gdm
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def service():
@@ -21,9 +25,17 @@ def service():
                 'migrate_to': migrate_to
             }
 
+        walker = Walker()
+        walker.from_address = migrate_from
+        walker.client_id = client_id
+        walker.client_secret = client_secret
+
+        db.session.add(walker)
+        try:
+            db.session.commit()
+        except IntegrityError as e:
+            db.session.rollback()
+
         print user_data
 
     return flask.render_template('index.html')
-
-def run():
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
